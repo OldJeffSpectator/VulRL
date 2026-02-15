726b895dd1ba861738bf0970cdbd624181bd9777
oldjeffspectator <oldjeffpsectator@gmail.com>
2026-02-15 15:24:00 +0800

    remove infra_v2 infra_v4

--- infra_v4/src/vulrl/env/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Environment module for Gymnasium-compliant interfaces."""
-
-from .security_env import SecurityEnv
-from .env_registry import EnvRegistry
-
-__all__ = ["SecurityEnv", "EnvRegistry"]
